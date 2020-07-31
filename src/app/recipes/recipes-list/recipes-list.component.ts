950c54465d94e726fb070405b57e538f7fc4d0b2
Arismendy Evangelista <ArismendyEZ115@hotmail.com>
2020-07-31 16:11:04 -0400

    Basic component creation.

--- src/app/recipes/recipes-list/recipes-list.component.ts
+++ src/app/recipes/recipes-list/recipes-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
 
 @Component({
   selector: 'app-recipes-list',
@@ -6,6 +7,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-list.component.css']
 })
 export class RecipesListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe("test","Another test","https://i.pinimg.com/736x/5e/8c/22/5e8c224446fef0df2ab8dd5d90a6a7fc.jpg"),
+    new Recipe("test #2","Test recipe No.2","http://assets.simplyrecipes.com/wp-content/uploads/2015/01/cheesy-tortellini-casserole-vertical-b-1600.jpg")
+  ];
 
   constructor() { }
 
